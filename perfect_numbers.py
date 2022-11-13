30aaa62c4004819dca0e32dc8763e9fa1ae7e71b
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-13 22:05:44 +0100

    Update perfect_numbers.py

--- perfect_numbers.py
+++ perfect_numbers.py
@@ -4,4 +4,15 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if (number <= 0):
+        raise ValueError ("Classification is only possible for positive integers.")
+    sum = 0
+    for x in range(1, int(number / 2)+1):
+        if (number % x == 0):
+            sum = sum + x
+    if (sum == number):
+        return "perfect"
+    elif (sum > number):
+        return " abundant"
+    else:
+        return "deficient"
